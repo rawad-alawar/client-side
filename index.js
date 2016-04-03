53cc15ddaef58102e2d67fc4b63724fb46346d04
Rawad Alawar <ralawar54@gmail.com>
2016-04-04 10:56:24 +1200

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('http://localhost:3000/v1/cats', function(err, data) {
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544s', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body)
+  console.log(data)
   document.body.innerHTML = example({ name: "Space" });
 })

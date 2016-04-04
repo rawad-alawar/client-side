78d2f46b876eea5f0e70648b0274de51477bb38a
Rawad Alawar <ralawar54@gmail.com>
2016-04-04 16:00:12 +1200

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-button.addEventListener("click", function(){ alert("something"); return false; },false);
+//button.addEventListener("click", function(){ alert("something"); return false; },false);
 xhr.get('https://api.wheretheiss.at/v1/satellites/25544s', function(err, data) {
   if (err) console.log(err) // do something
   var body = JSON.parse(data.body)

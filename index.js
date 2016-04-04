d1c7a81eec7a9ac00a54e10c6f4a1588b5531d47
Rawad Alawar <ralawar54@gmail.com>
2016-04-04 12:03:37 +1200

    Updates

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/satellites/25544s', function(err, data) {
   if (err) console.log(err) // do something
   var body = JSON.parse(data.body)
-  
-  document.body.innerHTML = example({ name: "Space", satID: "Satellite ID: "+ body.id, lat: "Latitude: "+ body.latitude, lon: "Longitude: "+ body.longitude });
+  document.body.innerHTML = example({ name: "Space", satID: "Satellite ID: "+ body.id, lat: "Latitude: "+ body.latitude, lon: "Longitude: "+ body.longitude, image: "http://static.bangordailynews.com/wp-content/blogs.dir/344/files/2015/10/iss.jpeg?strip=all?ref=inline" });
 })
